c61bba00f649e6c6cddfc84078a80a181117164c
Maxim <maksim.pantelidi@yandex.ru>
2023-04-29 17:11:49 +0300

    Logger. Create common contract #1

--- etc/config/global/file-path.php
+++ etc/config/global/file-path.php
@@ -3,12 +3,12 @@ define('DEBUG', 1);
 define('ROOT', dirname(__DIR__, 3));
 define('WWW', ROOT . '/public');
 define('APP', ROOT . '/app');
-define('CORE', ROOT . '/vendor/core');
-define('HELPERS', ROOT . '/vendor/core/helpers');
+define('CORE', ROOT . '/core');
+define('HELPERS', ROOT . '/core/helpers');
 define('CACHE', ROOT . '/tmp/cache');
 define('LOGS', ROOT . '/tmp/logs');
 define('ETC', ROOT . '/etc');
 
-// echo ROOT;
+echo CORE;
 
 require_once ROOT . '/vendor/autoload.php';
